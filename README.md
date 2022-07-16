09041a26a6a2520124512abdeccdbb81ef1607eb
Roman Munteanu <roma.mun@gmail.com>
2022-07-16 15:33:24 +0300

    Segmentio: reader and writer

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 kafka-go-demo
 -----
 
+## Docker container
+
 Run kafka container:
 ```
 docker-compose up -d
@@ -15,7 +17,7 @@ docker exec -it 3d35558a778e /bin/bash
 /opt/bitnami/kafka/bin
 ```
 
-Build and run
+## Build and Run
 ```
 go mod tidy
 
@@ -24,10 +26,31 @@ go mod vendor
 go run ./segmentio/main.go
 ```
 
-Kafka CLI
+## Kafka CLI
 ```
-docker exec -it 3d35558a778e /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
+docker exec -it 5a5abe94a0c3 /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
 
-docker exec -it 3d35558a778e /opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic my_topic
+docker exec -it 5a5abe94a0c3 /opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic my_topic
 
 ```
+
+
+## Resources
+
+### MSK
+https://aws.amazon.com/msk/what-is-kafka/
+https://docs.aws.amazon.com/msk/latest/developerguide/what-is-msk.html
+
+
+### Kafka container
+https://hub.docker.com/r/bitnami/kafka
+
+
+### Confluent
+https://github.com/confluentinc/confluent-kafka-go
+https://reposhub.com/go/miscellaneous/confluentinc-confluent-kafka-go.html
+https://github.com/confluentinc/confluent-kafka-go/blob/master/examples/stats_example/stats_example.go
+
+
+### Segmentio
+https://github.com/segmentio/kafka-go
--- segmentio/main.go
+++ segmentio/main.go
@@ -11,10 +11,13 @@ import (
 
 type App struct {
 	ctx       context.Context
+	host      string
 	topic     string
 	groupID   string
 	partition int
 	conn      *kafka.Conn
+	reader    *kafka.Reader
+	writer    *kafka.Writer
 }
 
 var app App
@@ -22,31 +25,52 @@ var app App
 func init() {
 	app = App{
 		ctx:       context.Background(),
+		host:      "localhost:9092",
 		topic:     "my_topic",
 		groupID:   "my_group_id",
 		partition: 0,
 	}
 
 	// assuming auto.create.topics.enable='true'
-	conn, err := kafka.DialLeader(app.ctx, "tcp", "localhost:9092", app.topic, app.partition)
+	conn, err := kafka.DialLeader(app.ctx, "tcp", app.host, app.topic, app.partition)
 	if err != nil {
 		log.Fatal("failed to dial leader:", err)
 	}
-
 	app.conn = conn
+
+	app.reader = kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{app.host},
+		GroupID:  app.groupID,
+		Topic:    app.topic,
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
+	})
+
+	// app.writer = kafka.NewWriter(kafka.WriterConfig{
+	// 	Brokers:  []string{app.host},
+	// 	Topic:    app.topic,
+	// 	Balancer: &kafka.LeastBytes{},
+	// })
+
+	app.writer = &kafka.Writer{
+		Addr:     kafka.TCP(app.host),
+		Topic:    app.topic,
+		Balancer: &kafka.LeastBytes{},
+	}
 }
 
 func main() {
+	defer app.Shutdown()
+
 	app.listTopics()
 
 	// app.produce()
 	// time.Sleep(3 * time.Second)
-	app.consume()
+	// app.consume()
 
-	err := app.conn.Close()
-	if err != nil {
-		log.Fatal("failed to close connection:", err)
-	}
+	app.writeMessages()
+	time.Sleep(3 * time.Second)
+	app.readMessages()
 }
 
 func (a *App) consume() {
@@ -98,5 +122,53 @@ func (a *App) listTopics() {
 
 	for k := range m {
 		fmt.Println(k)
+	}
+}
+
+func (a *App) readMessages() {
+	for {
+		m, err := a.reader.ReadMessage(a.ctx)
+		if err != nil {
+			break
+		}
+		fmt.Println("-------------- msg:")
+		fmt.Println("Topic:", m.Topic)
+		fmt.Println("Partition:", m.Partition)
+		fmt.Println("Offset:", m.Offset)
+		fmt.Println("Key:", string(m.Key))
+		fmt.Println("Value:", string(m.Value))
+	}
+}
+
+func (a *App) writeMessages() {
+	err := a.writer.WriteMessages(a.ctx,
+		kafka.Message{
+			Key:   []byte("routingKey1"),
+			Value: []byte("message 1"),
+		},
+		kafka.Message{
+			Key:   []byte("routingKey2"),
+			Value: []byte("message 2"),
+		},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+}
+
+func (a *App) Shutdown() {
+	err := a.conn.Close()
+	if err != nil {
+		log.Fatal("failed to close connection:", err)
+	}
+
+	err = a.reader.Close()
+	if err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
+
+	err = a.writer.Close()
+	if err != nil {
+		log.Fatal("failed to close writer:", err)
 	}
 }
